c692b8c41c1cc54c4a673756232f2fd65b31d8fa
burhanbile <burhanbile103@gmail.com>
2017-06-09 17:22:45 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,18 @@
+##PROJECT NAME:
+PING PONG
+
+##AUTHOR:
+BURHAN AHMED
+
+##PROJECT DESRIPTION
+The project is a web application that takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:
+
+Numbers divisible by 3 are replaced with "ping"
+Numbers divisible by 5 are replaced with "pong"
+Numbers divisible by 15 are replaced with "pingpong"
+
+##specifications
+the web application will take up user input (in this case a number)
+and count up to the number<br>
+ Example Input 2
+ Example output[1,2]</br>
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 //Business logic
-var pingPong = function(userInput) {
-  var results = [];
-  for (var x = 1; x <= userInput; x++) {
-    if (x % 15 === 0) {
-      results.show("pingpong");
-    } else if (x % 5 === 0) {
-      results.show("pong");
-    } else if (x % 3 === 0) {
-      results.show("ping");
-    } else {
-      results.show(x);
+ var pingPong = function (userInput) {
+    var results = [];
+    for (var x = 1; x <= userInput; x++) {
+        if (x % 15 === 0) {
+            results.push("pingpong");
+        } else if (x % 5 === 0) {
+                results.push("pong");
+        } else if (x % 3 === 0) {
+                results.push("ping");
+        } else {
+            results.push(x);
+        }
     }
-  }
-  return results;
-};
+    return results;
+  };
 //user interface
 $(document).ready(function() {
   $("form#Ping-Pong").submit(function(event) {
